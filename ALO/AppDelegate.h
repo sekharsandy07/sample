f8fa6ec69bc08b3babd348ef7045b94ef4709439
Sandy Sekhar <Sandy@sandys-macbook-pro.local>
2017-09-02 13:22:24 +0530

    Initial Commit

---
+++ ALO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALO
+//
+//  Created by Sandy Sekhar on 9/2/17.
+//  Copyright © 2017 Sandy.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
